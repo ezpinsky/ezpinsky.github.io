3c52837b279fa4deaba7ef29f563f77539450cb5
ezpinsky <ezpinsky@gmail.com>
2021-04-13 18:14:23 -0400

    adds contact me form

---
+++ contactform.php
@@ -0,0 +1,12 @@
+<!-- <?php
+$email = $_POST['email']
+$name = $_POST['name']
+$subject = $_POST['subject']
+$message = $_POST['message']
+
+if($_POST["message"]) {
+mail("ezpinsky@gmail.com", $subject, $message,"From: $email \r\n");
+}
+header("Location: http://www.ezrapinsky.dev");
+
+?> -->
